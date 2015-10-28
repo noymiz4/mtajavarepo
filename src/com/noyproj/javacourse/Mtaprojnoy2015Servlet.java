d2d5aa4d02edf3098578853a85bf5e54c57da696
noymiz4 <noymiz4@gmail.com>
2015-10-28 23:33:36 +0200

    third

--- src/com/noyproj/javacourse/Mtaprojnoy2015Servlet.java
+++ src/com/noyproj/javacourse/Mtaprojnoy2015Servlet.java
@@ -6,6 +6,6 @@ import javax.servlet.http.*;
 public class Mtaprojnoy2015Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, Noy");
+		resp.getWriter().println("Hello, Noy!!");
 	}
 }
